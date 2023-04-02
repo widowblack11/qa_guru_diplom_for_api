39ce7cbb8d0ebef125dd181e388fcb7e8140c38e
widowblack11 <oksana.prokopenko16@gmail.com>
2023-04-02 12:39:38 +0300

    add run on selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
 import os
 
-import allure
+from selenium import webdriver
 import pytest
 from dotenv import load_dotenv
 from selene.support.shared import browser
-
+from selenium.webdriver.chrome.options import Options
 from framework.DemoQaWithEnv import DemoQaWithEnv
+from utils import attach
 
 
+authorization_cookie = None
 load_dotenv()
+DEFAULT_BROWSER_VERSION = "100.0"
 
 
 def pytest_addoption(parser):
@@ -30,26 +33,35 @@ def reqres(get_option):
     return DemoQaWithEnv(get_option).reqres
 
 
-@pytest.fixture(scope='session')
-def cookie(demoshop):
-    response = demoshop.login(os.getenv("LOGIN"), os.getenv("PASSWORD"))
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
-
-    with allure.step("Check code"):
-        assert response.status_code == 302
-
-    return authorization_cookie
-
-
 @pytest.fixture(scope='function')
-def auth_browser(demoshop, cookie):
+def auth_browser(demoshop):
+    global authorization_cookie
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN_SELENOID')
+    password = os.getenv('PASSWORD_SELENOID')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
     browser.config.base_url = demoshop.demoqa.url
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    if authorization_cookie is None:
+        response = demoshop.login(os.getenv("LOGIN"), os.getenv("PASSWORD"))
+        authorization_cookie = response.cookies.get('NOPCOMMERCE.AUTH')
     browser.open("/Themes/DefaultClean/Content/images/logo.png")
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
-
+    browser.driver.add_cookie({"name": 'NOPCOMMERCE.AUTH', "value": authorization_cookie})
     yield browser
-    browser.quit()
-
-
+    attach.add_screenshot(browser)
+    attach.add_video(browser)
 
 
--- tests/ui_plus_api/test_for_login.py
+++ tests/ui_plus_api/test_for_login.py
@@ -23,21 +23,6 @@ def test_login_with_api(auth_browser):
         auth_browser.element(".account").should(have.text(os.getenv("LOGIN")))
 
 
-@allure.title('Разлогин')
-@allure.tag('api_plus_api')
-@allure.label('owner', 'o_prokopenko')
-@allure.severity(Severity.CRITICAL)
-@allure.feature('Разлогин')
-@allure.story('demowebshop')
-def test_logout(auth_browser):
-    with allure.step('Открыть главную страницу под залогининым пользователем'):
-        auth_browser.open("")
-
-    with allure.step('Нажать кнопку "Log out"'):
-        browser.element('.ico-logout').click()
-    with allure.step('Проверить, что пользователь разлогинился'):
-        browser.element('.ico-login').should(have.text('Log in'))
-
 
 
 
